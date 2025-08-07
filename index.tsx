4a1b5a620dd560ee5dcd9480c263128d03b133f8
MarcosRMoraesJr <91978111+MarcosRMoraesJr@users.noreply.github.com>
2025-08-07 16:33:52 -0300

    fix: Atualização imagem do topo LABirintar

--- index.tsx
+++ index.tsx
@@ -418,8 +418,7 @@ const TopBar: React.FC = () => {
       <div className="w-full bg-white shadow-sm fixed top-0 z-50">
         <div className="max-w-7xl mx-auto px-4 py-2 flex items-center justify-between">
           <div className="flex items-center space-x-2">
-            <img src="logoslabirintar/Labirintar_RGB.png" alt="Logo" className="h-6" />
-            <span className="font-bold text-secondary">LABIRINTAR</span>
+            <img src="logoslabirintar/Labirintar_Bubble.webp" alt="Logo LABirintar" className="h-6" />
           </div>
 
           <div className="hidden md:flex items-center space-x-4">
